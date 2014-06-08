9d936342bd39739d68a225f93f39f24e8275784d
amusinglogic <amusinglogic@gmail.com>
2014-06-08 02:06:56 -0400

    comment 5

--- problem5.rb
+++ problem5.rb
@@ -1,15 +1,11 @@
+#setting var value
 i = 20
 
+#until loop with range of integers 1-20
+#going by multiples of 20 since it has to be divisible
 
 until (1..20).all? { |x| i % x == 0 }
   i += 20
   end
 
 puts i
-
-
-   
-  
-    
-    
-  
