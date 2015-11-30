990f6c11ec1ea4952f183ea21655c4851151e711
jptime <amusinglogic@gmail.com>
2015-11-30 17:58:14 +0000

    problem 4 finished

---
+++ JS/problem4.js
@@ -0,0 +1,11 @@
+var start = 100;
+var end = 999;
+var winners = [];
+
+for (var i= start; i <= end; i++){
+  for (var j = end; j >= start; j--){
+   var temp = i * j
+   
+   }
+}
+console.log(temp)
