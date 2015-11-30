2a76db7db53bb2a255b97004f7c0020e728e32e2
jptime <amusinglogic@gmail.com>
2015-11-30 22:00:24 +0000

    problem 6 added

--- JS/problem6.js
+++ JS/problem6.js
@@ -1,8 +1,11 @@
 var sum1 = 0;
-
+var sum2 = 0;
+var result = 0;
 for (var i=1; i<=100; i++){
+  sum2 += i
   sum1+= Math.pow(i, 2);
   
 }
-sum1 = Math.pow(sum1, 2)
-console.log(sum1)
+sum2 = Math.pow(sum2, 2)
+result = sum2-sum1
+console.log(result)
