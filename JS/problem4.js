f1707d8e183ebbd64d85d3a9f93484eef3138bc0
jptime <amusinglogic@gmail.com>
2015-11-30 19:17:22 +0000

    problem 4 finished

--- JS/problem4.js
+++ JS/problem4.js
@@ -1,11 +1,17 @@
 var start = 100;
 var end = 999;
 var winners = [];
+var temp=[];
 
 for (var i= start; i <= end; i++){
   for (var j = end; j >= start; j--){
-   var temp = i * j
+   temp = (i * j)
+   if (temp.toString().split('').reverse().join('') === temp.toString()){
+   
+   winners.push(temp);
+     
+    }
    
    }
 }
-console.log(temp)
+console.log(winners.sort(function(a, b){return a-b}).pop())
